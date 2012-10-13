7e4c4da2c12ffc30229073559c257cf512653bbb
cgack <cory.gack@gmail.com>
2012-10-13 16:13:41 -0400

    updates

--- cfitlog.py
+++ cfitlog.py
@@ -1,14 +1,10 @@
-import cgi
-import urllib
 import os
 import json
-import wsgiref.handlers
-
+import logging
 
 from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.api import users
-from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
 from model import WorkoutDefinition
@@ -65,45 +61,60 @@ def _datetime_from_str(time_str):
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-
+        workouts = []
+        definitions = []
+        logging.info("main page" + self.request.url)
         if users.get_current_user():
+            wkouts = db.GqlQuery("SELECT * FROM WorkoutLog WHERE owner = :u ORDER BY date DESC", u=users.get_current_user())
+            defs = db.GqlQuery("SELECT * FROM WorkoutDefinition WHERE owner = :u", u=users.get_current_user())
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
             picklink = 2
         else:
+            wkouts = ""
+            defs = ""
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
             picklink = 1
+
+        for wkout in wkouts:
+            workouts.append({'desc': wkout.description, 'result': wkout.result, 'date': str(wkout.date.replace(microsecond=0))})
+
+        for deff in defs:
+            definitions.append({'desc': deff.description, 'logBy': deff.logBy})
 
         template_values = {
             'url': url,
             'url_linktext': url_linktext,
             'picklink': picklink,
+            'workouts': workouts,
+            'definitions': definitions
         }
 
-        path = os.path.join(os.path.dirname(__file__), 'templates/home.html')
+        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
         self.response.out.write(template.render(path, template_values))
 
 
 class Define(webapp.RequestHandler):
     def get(self):
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-            picklink = 2
+        if self.request.get('logBy') != "":
+            logging.info("define")
+            q = self.request.get('workout')
+            by = int(self.request.get('logBy'))
+            workout = WorkoutDefinition()
+            if users.get_current_user():
+                workout.owner = users.get_current_user()
+
+            workout.description = q
+            workout.logBy = by
+
+            workout.put()
+
+            response_data = {"workout": q, "logBy": by}
+            self.response.out.write(json.dumps(response_data))
         else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-            picklink = 1
-
-        template_values = {
-            'url': url,
-            'url_linktext': url_linktext,
-            'picklink': picklink,
-        }
-
-        path = os.path.join(os.path.dirname(__file__), 'templates/generate.html')
-        self.response.out.write(template.render(path, template_values))
+            response_data = {"error": "there was a problem storing your workout"}
+            self.response.out.write(json.dumps(response_data))
 
 
 class PickWorkout(webapp.RequestHandler):
@@ -124,6 +135,21 @@ class PickWorkout(webapp.RequestHandler):
         for wkout in wkouts:
             workouts.append({'desc': wkout.description, 'logBy': wkout.logBy})
 
+        if self.request.get('result') != "":
+            desc = self.request.get('workout')
+            res = self.request.get('result')
+            date = _datetime_from_str(self.request.get('date'))
+            workout = WorkoutLog()
+
+            if users.get_current_user():
+                workout.owner = users.get_current_user()
+
+            workout.description = desc
+            workout.result = res
+            workout.date = date
+
+            workout.put()
+
         template_values = {
             'url': url,
             'url_linktext': url_linktext,
@@ -133,10 +159,12 @@ class PickWorkout(webapp.RequestHandler):
 
         path = os.path.join(os.path.dirname(__file__), 'templates/pick.html')
         self.response.out.write(template.render(path, template_values))
+        # return HttpResponse(json.dumps(jsondata), mimetype="application/json")
 
 
 class Generate(webapp.RequestHandler):
     def post(self):
+        logging.info("generate")
         q = self.request.get('workout')
         by = int(self.request.get('logBy'))
         workout = WorkoutDefinition()
--- js/script.js
+++ js/script.js
@@ -1,45 +1,48 @@
 
-$(document).ready(function() {
+		$(document).one("pageinit", function() {
 
+				
+		$("#generateWkout").on("vclick", function() {
+				var wkout = $("#workout").val();
+				var logBy = $("#logBy").val();
+				var workout = {};
+				workout.workout = wkout;
+				workout.logBy = logBy;
+
+				$.ajax({
+					type: "GET",
+					url: "/define",
+					data: workout,
+					success: function() {
+						$("#workout").val("");
+						$("#wkouts").append("<option value='" + workout.workout + "'> " + workout.workout + "</option>");
+					}
+				});
+				return false;
+			});
 		
-	$("#generateWkout").click(function() {
-		var wkout = $("#workout").val();
-		var logBy = $("#logBy").val();
-		var workout = {};
-		workout.workout = wkout;
-		workout.logBy = logBy;
+			$("#storeWkout").on( "vclick", function() {
+				var wkout = $("#wkouts option:selected").html();
+				var result = $("#results").val();
+				var date = $("#date").val() + " 00:00:00";
+				var workout = {};
+				workout.workout = wkout;
+				workout.result = result;
+				workout.date = date;
+				$.ajax({
+					type: "POST",
+					url: "/store",
+					data: workout,
+					success: function() {
+						//location.href = "/";
+						var li = "<li class='well'><h3>" + wkout + "</h3><p>" + result + "</p><p class='ui-li-aside'>" + date + "</p></li>";
+						$("#loggedWk").prepend(li);
+						$("#loggedWk").listview("refresh");
+						$.mobile.changePage("#history");
+					}
+				});
+				return false;
+			});
 
-		$.ajax({
-			type: "POST",
-			url: "/generate",
-			data: workout,
-			success: function() {
-				$("#workout").val("");
-			}
+
 		});
-		return false;
-	});
-
-	$("#storeWkout").click(function() {
-		var wkout = $("#wkouts option:selected").html();
-		var result = $("#results").val();
-		var date = $("#date").val() + " 00:00:00";
-		var workout = {};
-		workout.workout = wkout;
-		workout.result = result;
-		workout.date = date;
-		$.ajax({
-			type: "POST",
-			url: "/store",
-			data: workout,
-			success: function() {
-				location.href = "/";
-			}
-		});
-		return false;
-	});
-
-
-});
-
-
